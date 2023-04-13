721be4fc9c03bd4a607c5afe95b4fb813a7d1997
Re-Programmed <reprogrammed.yt@gmail.com>
2023-04-12 19:56:40 -0600

    Animation Boilerplate.

---
+++ Utils/Animatr/Animation.cs
@@ -0,0 +1,28 @@
+﻿using _2DGameMaker.Objects;
+using _2DGameMaker.Objects.Scripting;
+using _2DGameMaker.Rendering.Sprites;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2DGameMaker.Utils.Animatr
+{
+    public abstract class Animation : ObjectAppendedScript
+    {
+        private readonly Texture2D[] frames;
+        private int currentFrame = 0;
+
+        public Animation(GameObject gameObject, Texture2D[] frames)
+            : base(gameObject)
+        {
+            this.frames = frames;
+        }
+
+        protected void advanceFrame()
+        {
+            currentFrame++;
+            if (currentFrame == frames.Length) { currentFrame = 0; }
+            gameObject.SetTexture(new ObjectTexture(frames[currentFrame], gameObject.Texture.GetFlipped(), gameObject.Texture.GetColor(), gameObject.Texture.GetRotation()));
+        }
+    }
+}
---
+++ Utils/Animatr/TimeRelativeAnimation.cs
@@ -0,0 +1,47 @@
+﻿using _2DGameMaker.Game;
+using _2DGameMaker.Objects;
+using _2DGameMaker.Rendering.Sprites;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2DGameMaker.Utils.Animatr
+{
+    public class TimeRelativeAnimation : Animation
+    {
+        readonly float speed;
+        float T;
+
+        public TimeRelativeAnimation(GameObject gameObject, float speed, Texture2D[] textures)
+            : base(gameObject, textures)
+        {
+            this.speed = speed;
+        }
+
+        public TimeRelativeAnimation(GameObject gameObject, string speed, string[,] textures)
+            : base(gameObject, AnimationBuilder.BuildT2D(textures))
+        {
+            this.speed = float.Parse(speed); 
+        }
+
+        protected override void destroy()
+        {
+            
+        }
+
+        protected override void init()
+        {
+
+        }
+
+        protected override void update()
+        {
+            T += GameTime.NormalizedDeltaTime();
+
+            if(T >= speed)
+            {
+                advanceFrame();
+            }
+        }
+    }
+}
